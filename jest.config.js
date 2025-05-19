02d0c774b6dac43a179d04b695ca40ce9a29e5e8
Victor Milu <victor.milu@kxp.consulting>
2025-05-19 15:23:04 -0300

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    preset: 'react-native',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testMatch: ['**/src/test/**/*.test.(ts|tsx)'],
-    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-    transformIgnorePatterns: [
-      'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/'
-    ],
-    collectCoverageFrom: [
-      'src/**/*.{ts,tsx}',
-      '!**/node_modules/**',
-      '!**/types/**',
-      '!**/__mocks__/**'
-    ]
-  };
+  preset: 'react-native',
+  testMatch: ['**/src/test/**/*.test.ts?(x)'],
+};
