bc9782d50a2cd324d90dea964f131ffa9a6824d3
Victor Milu <victor.milu@kxp.consulting>
2025-05-19 16:19:36 -0300

    add: tests in app

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: 'react-native',
-  testMatch: ['**/src/test/**/*.test.ts?(x)'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|@react-navigation' +
+      '|react-native-vector-icons' +
+      '|@react-native-community' +
+    ')/)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
